224e094255c4115b4f9f2165385ad81d0d335079
Himani Acharya <himaniacharya1@gmail.com>
2024-04-26 10:41:23 +0545

    I was here

--- FrontEnd/bisleriumBlog/src/App.jsx
+++ FrontEnd/bisleriumBlog/src/App.jsx
@@ -4,7 +4,8 @@ import Login from "../Components/Login";
 function App() {
   return (
     <div className="flex-col h-screen items-center">
-      <h1 className="text-6xl font-semibold text-center">HRSK TAILWIND TEST</h1>
+      <h1 className="text-6xl font-semibold text-center">HRSK Pvt. Ltd. TAILWIND TEST</h1>
+      <h1 className="text-6xl font-semibold text-center">Himani i was here</h1>
       <Login></Login>
     </div>
   );
