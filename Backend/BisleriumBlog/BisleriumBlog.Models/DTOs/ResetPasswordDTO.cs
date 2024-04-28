7596bcdd28ecd36e6f0da2d6b50ec9e5ff675c51
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-04-28 21:14:23 +0545

    Completed User Profile Update and Forgot Password Reset

---
+++ Backend/BisleriumBlog/BisleriumBlog.Models/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BisleriumBlog.Models.DTOs
+{
+    public class ResetPasswordDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = string.Empty;
+        [Required]
+        public string Token { get; set; }
+
+    }
+}
