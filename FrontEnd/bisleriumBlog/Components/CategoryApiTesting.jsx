c61416c9f349741cc9ac9cdb667c763593b440da
Hrishik Sangroula <hrishiksangroul@gmail.com>
2024-05-04 17:06:02 +0545

    create category

--- FrontEnd/bisleriumBlog/Components/CategoryApiTesting.jsx
+++ FrontEnd/bisleriumBlog/Components/CategoryApiTesting.jsx
@@ -3,19 +3,20 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Categoryurl } from "../src/index";
 
-// Don;t use this  or ovveride this  , this jsx is for testing category API, creat another jsx  
+// Don;t use this  or ovveride this  , this jsx is for testing category API, creat another jsx
 const Category = () => {
   const token = useSelector((state) => state.signin.token);
   const [categorydata, setcategorydata] = useState([]);
-  const Requestoptions = {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + token,
-    },
-  };
+  const [Name, setName] = useState("");
 
   const listCategory = async (e) => {
     e.preventDefault();
+    const Requestoptions = {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
     const response = await fetch(
       Categoryurl + "GetAllCategory",
       Requestoptions
@@ -26,6 +27,28 @@ const Category = () => {
       setcategorydata(data.result.category);
     }
   };
+
+  const addcategory = async (e) => {
+    const formData = new FormData();
+    formData.append("Name", Name.trim());
+    console.log(Name)
+    const Requestoptions = {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      body: formData,
+    };
+    const response = await fetch(
+      Categoryurl + "CreateCategory",
+      Requestoptions
+    );
+     const data = await response.json();
+     if (response.status == 200) {
+       console.log(data)
+     }
+  };
+
   return (
     <>
       <button type="button" onClick={listCategory}>
@@ -34,13 +57,24 @@ const Category = () => {
       {categorydata.length == 0 ? (
         <p>Please Click to see data</p>
       ) : (
-        categorydata.map((category) => 
-
-          (<div>
+        categorydata.map((category,index) => (
+          <div key={index}>
             <h1>{category.name}</h1>
-          </div>)
-        )
+          </div>
+        ))
       )}
+
+      <input
+        type="text"
+        name="category"
+        id="category"
+        placeholder="Enter category"
+        value={Name}
+        onInput={(e) => setName(e.target.value)}
+      />
+      <button type="button" onClick={addcategory}>
+        Category Add{" "}
+      </button>
     </>
   );
 };
