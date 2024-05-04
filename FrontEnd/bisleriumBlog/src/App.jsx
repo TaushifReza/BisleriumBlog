fa13c163e8a0519d03fcf7c9b0c5a74cbf77f685
Himani Acharya <himaniacharya1@gmail.com>
2024-05-04 22:51:48 +0545

    .

--- FrontEnd/bisleriumBlog/src/App.jsx
+++ FrontEnd/bisleriumBlog/src/App.jsx
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
+
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import MyState from "../context/myState";
+import Category from "../Components/CategoryApiTesting";
 
 function App({ children }) {
   return (
     <MyState>
       <div>
         {children}  {/* Child components will be the routed pages */}
+        <Category></Category>
         <Toaster />
       </div>
     </MyState>
-=======
-import Category from "../Components/CategoryApiTesting";
-function App() {
-  return (
-    <>
-
-      <h1 className="text-4xl text-center mt-10">Under Construction</h1>
-      <Category></Category>
-    </>
->>>>>>> fa224424805751795766b7694294a5a4ef08041c
-  );
+  )
 }
 
 export default App;
