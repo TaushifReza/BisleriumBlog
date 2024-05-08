fc7e7267bbf474f69d2aca97262f7349c00fb95b
Bicky Yadav <yadavbicky99@gmail.com>
2024-05-08 20:33:21 +0545

    Top 10 Blog table admindashboard

--- FrontEnd/bisleriumBlog/Components/AdminDashboard.jsx
+++ FrontEnd/bisleriumBlog/Components/AdminDashboard.jsx
@@ -179,6 +179,100 @@ function AdminDashboard() {
                                 </div>
                             </div>
                         </div>
+                        <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <h4 class="card-title">Top 10 Blogs</h4>
+                                        <div class="table-responsive">
+                                            <table class="table table-centered table-striped table-nowrap mb-0">
+                                                <thead>
+                                                    <tr>
+                                                        <th>User</th>
+                                                        <th>Email</th>
+                                                        <th>Blog title</th>
+                                                        <th>Create Date</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <a href="javascript:void(0);" class="text-body font-weight-semibold">Paul J. Friend</a>
+                                                        </td>
+                                                        <td>
+                                                            pauljfrnd@jourrapide.com
+                                                        </td>
+                                                        <td>
+                                                            Title 1
+                                                        </td>
+                                                        <td>
+                                                            07/07/2018
+                                                        </td>
+                                                    </tr>
+                                                    
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <a href="javascript:void(0);" class="text-body font-weight-semibold">Bryan J. Luellen</a>
+                                                        </td>
+                                                        <td>
+                                                            bryuellen@dayrep.com
+                                                        </td>
+                                                        <td>
+                                                            Title 2
+                                                        </td>
+                                                        <td>
+                                                            09/12/2018
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <a href="javascript:void(0);" class="text-body font-weight-semibold">Kathryn S. Collier</a>
+                                                        </td>
+                                                        <td>
+                                                            collier@jourrapide.com
+                                                        </td>
+                                                        <td>
+                                                            Title 3
+                                                        </td>
+                                                        <td>
+                                                            06/30/2018
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <a href="javascript:void(0);" class="text-body font-weight-semibold">Timothy Kauper</a>
+                                                        </td>
+                                                        <td>
+                                                            thykauper@rhyta.com
+                                                        </td>
+                                                        <td>
+                                                            Title 4
+                                                        </td>
+                                                        <td>
+                                                            09/08/2018
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <a href="javascript:void(0);" class="text-body font-weight-semibold">Zara Raws</a>
+                                                        </td>
+                                                        <td>
+                                                            austin@dayrep.com
+                                                        </td>
+                                                        <td>
+                                                            Germany
+                                                        </td>
+                                                        <td>
+                                                            07/15/2018
+                                                        </td>
+                                                    </tr>
+                                                    
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
                
@@ -198,6 +292,9 @@ function AdminDashboard() {
                 </footer>
 
             </div>
+
+            
+
 
 
 
