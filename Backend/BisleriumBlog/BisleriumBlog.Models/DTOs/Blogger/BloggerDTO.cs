6ab5e40693b5b842149201cb3673b98ab241e6c9
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-05-13 20:39:07 +0545

    Create an API to GET Top 10 Blogger

---
+++ Backend/BisleriumBlog/BisleriumBlog.Models/DTOs/Blogger/BloggerDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BisleriumBlog.Models.DTOs.Blogger
+{
+    public class BloggerDTO
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public int TotalUpvotes { get; set; }
+        public int TotalBlogs { get; set; }
+    }
+}
